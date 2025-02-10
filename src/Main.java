6fd0c2b5cb3699f68873d6480e7d893446496ab0
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-10 13:50:38 -0500

    i

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Apple");
+        arrayList.add("Banana");
+        arrayList.add("Cherry");
+        arrayList.add("Mango");
+        arrayList.add("Strawberry");
+
+        System.out.println("ArrayList: " + arrayList);
+    }
 }
